0d925679fea676fc0a24f47db5e0434198a780ba
gelvezz23 <gelvezz223@gmail.com>
2020-04-13 02:45:53 -0500

    Redux/add style and function app

--- Redux/blog/src/components/App.js
+++ Redux/blog/src/components/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 
+
+
+
 const App = () => {
+
+  const ponerFilas = () =>[
+    <tr>
+      <td>Carlos</td>
+      <td>carlos@devhome.dev</td>
+      <td>devhome.dev</td>
+    </tr>,
+    <tr>
+      <td>Carlos</td>
+      <td>carlos@devhome.dev</td>
+      <td>devhome.dev</td>
+    </tr>
+  ]
+
   return (
     <div className="margen">
       <table className="tabla">
           <thead>
             <tr>
-              <td>nombre</td>
-              <td>Correo</td>
-              <td>Enlace</td>
+              <th>nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Carlos</td>
-              <td>carlos@devhome.dev</td>
-              <td>devhome.dev</td>
-            </tr>
-            <tr>
-              <td>Carlos</td>
-              <td>carlos@devhome.dev</td>
-              <td>devhome.dev</td>
-            </tr>
+            {ponerFilas()}
           </tbody>
       </table>
     </div>
