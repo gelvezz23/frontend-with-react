788c296284e5d680ac474b7908edb2cfd322e2ab
gelvezz23 <gelvezz223@gmail.com>
2020-04-16 03:30:13 -0500

    Redux/ add files Types for reducers and actions

--- Redux/blog/src/reducers/usuarioReducer.js
+++ Redux/blog/src/reducers/usuarioReducer.js
@@ -1,9 +1,11 @@
+import {TRAER_TODOS} from './../types/usuariosTypes'
+
 const INITIAL_STATE = {
     usuario:[]
 };
 export default (state = INITIAL_STATE, action ) =>{
     switch(action.type){   
-        case 'traer_usuario':
+        case TRAER_TODOS:
             return {...state, usuario:action.payload}
         default: return state
     }
