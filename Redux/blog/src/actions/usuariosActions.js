5198df4e2e9aedebf7d68b7b3ddfa6fd09953047
gelvezz23 <gelvezz223@gmail.com>
2020-04-16 03:36:07 -0500

    Redux/ add errors with try and catch

--- Redux/blog/src/actions/usuariosActions.js
+++ Redux/blog/src/actions/usuariosActions.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
+import {TRAER_TODOS} from './../types/usuariosTypes'
+
 
 export const traerTodos = () => async (dispatch) =>{
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        dispatch({
+            type: TRAER_TODOS,
+            payload: response.data
+        })
+    }catch(error){
+        console.log('Error : '+ error.message)
 
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-    dispatch({
-        type: 'traer_usuario',
-        payload: response.data
-    })
+    }
+    
 }
