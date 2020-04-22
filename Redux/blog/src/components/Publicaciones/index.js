9e9f53f5a39cd652bc156530d9ce8aa835899a84
gelvezz23 <gelvezz223@gmail.com>
2020-04-21 22:09:55 -0500

    Redux/ destructuring and rename in publicaciones (function actions)

--- Redux/blog/src/components/Publicaciones/index.js
+++ Redux/blog/src/components/Publicaciones/index.js
@@ -1,35 +1,45 @@
-import React, {Component} from 'react'
-import { connect } from 'react-redux'
-import * as usuarioActions from '../../actions/usuariosActions'
-import './../../css/index.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as usuariosActions from '../../actions/usuariosActions';
+import * as publicacionesActions from '../../actions/publicacionesActions';
+
+
+const {traerTodos: usuariosTraerTodo} = usuariosActions
+const {traerTodos: publicacionesTraerTodo} = publicacionesActions
 
 
 class Publicaciones extends Component {
-    componentDidMount(){
-        if(!this.props.usuarioReducer.usuario.length){
-            this.props.traerTodos()
-        }
-    }
-    
 
+	componentDidMount() {
+		if (!this.props.usuarioReducer.usuario.length) {
+			this.props.traerTodos();
+		}
+	}
 
-    render(){
-        console.log(this.props)
-        return(
-            <div className="margen">
-                <h1>Publicaciones de </h1>
-                {this.props.match.params.key}
-            </div>
-        )
-    }
+	render() {
+		console.log(this.props);
+		return (
+			<div className="margen">
+				<h1>
+					Publicaciones de 
+				</h1>
+				{ this.props.match.params.key }
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = ({usuarioReducer, PublicacionesReducer}) => {
-    return {
+const mapStateToProps = ({ usuarioReducer, publicacionesReducer }) => {
+    return { 
         usuarioReducer, 
-        PublicacionesReducer
-    }
-  
-  }
+        publicacionesReducer 
+    };
+};
 
-export default connect(mapStateToProps,usuarioActions)(Publicaciones);
+const mapDispatchToProps = {
+    usuariosTraerTodo,
+    publicacionesTraerTodo
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
