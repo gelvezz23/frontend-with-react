99194d9a28fa40dcacf9a7ae85f9c837fa1ca420
gelvezz23 <gelvezz223@gmail.com>
2020-05-17 08:42:04 -0500

    add task normalitation

--- Redux/blog/src/actions/tareasActions.js
+++ Redux/blog/src/actions/tareasActions.js
@@ -10,6 +10,16 @@ export const traerTodas = () => async (dispatch) => {
 
 	try {
 		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/todos');
+
+		const tareas = {};
+		respuesta.data.map(
+			(tar) =>
+				(tareas[tar.userId] = {
+					...tareas[tar.userId],
+					[tar.id]: { ...tar },
+				})
+		);
+
 		dispatch({
 			type: TRAER_TODAS,
 			payload: respuesta.data,
